3ae24a184852639bf661213570b5bf8ec966053f
Rishusingh18 <rs9354403038@gmail.com>
2026-08-16 14:55:48 +0530

    Fix: Add compiled CSS link to all secondary and city pages to restore styling

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,9 @@ export default defineConfig({
   plugins: [
     react(),
     {
-      name: 'non-blocking-css',
+      name: 'optimize-preload',
       transformIndexHtml(html) {
-        return html
-          .replace(
-            /<link rel="stylesheet" crossorigin href="([^"]+\.css)">/g,
-            '<link rel="preload" as="style" href="$1" onload="this.onload=null;this.rel=\'stylesheet\'"><noscript><link rel="stylesheet" href="$1"></noscript>'
-          )
-          .replace(/<link rel="modulepreload" crossorigin href="\/assets\/vendor-data-[^"]+\.js">\n?/g, '');
+        return html.replace(/<link rel="modulepreload" crossorigin href="\/assets\/vendor-data-[^"]+\.js">\n?/g, '');
       }
     }
   ],
