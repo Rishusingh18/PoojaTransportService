0efd2915b79ba24c8251ae6198d38c3f34785f0c
Rishusingh18 <rs9354403038@gmail.com>
2026-08-16 14:37:39 +0530

    Perf: Remove eager modulepreload of heavy Supabase chunk from initial HTML head

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,12 @@ export default defineConfig({
     {
       name: 'non-blocking-css',
       transformIndexHtml(html) {
-        return html.replace(
-          /<link rel="stylesheet" crossorigin href="([^"]+\.css)">/g,
-          '<link rel="preload" as="style" href="$1" onload="this.onload=null;this.rel=\'stylesheet\'"><noscript><link rel="stylesheet" href="$1"></noscript>'
-        );
+        return html
+          .replace(
+            /<link rel="stylesheet" crossorigin href="([^"]+\.css)">/g,
+            '<link rel="preload" as="style" href="$1" onload="this.onload=null;this.rel=\'stylesheet\'"><noscript><link rel="stylesheet" href="$1"></noscript>'
+          )
+          .replace(/<link rel="modulepreload" crossorigin href="\/assets\/vendor-data-[^"]+\.js">\n?/g, '');
       }
     }
   ],
